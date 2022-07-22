8f50934f3f39e0a2510af225de046588208a59af
rzamaraev <rzamaraev@fix.ru>
2022-07-22 17:44:30 +0300

    day 6 form validation form styles

--- calories_counter/calories/forms.py
+++ calories_counter/calories/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Name", initial="Nekit", help_text="Please enter your name")
-    age = forms.IntegerField(label="Age", help_text="Please enter your age")
+    name = forms.CharField(label="Name", help_text="Please enter your name", max_length=7)
+    age = forms.IntegerField(label="Age", help_text="Please enter your age", min_value=1)
     comment = forms.CharField(label="Comments", widget=forms.Textarea, initial="SVINOTA", help_text="If you want you "
                                                                                                     "may")
     field_order = ["age", "name", "comment"]
